0082fa60ad1a8f704c310353ee7345c3c6e5f2b4
0x95 <63484952+XYNHY@users.noreply.github.com>
2022-04-19 18:59:52 -0400

    Update Soft.lua

--- Aimbot/Soft.lua
+++ Aimbot/Soft.lua
@@ -1,11 +1,10 @@
-local HitBoxSize=6
-local HitBoxTarget="Torso"
-game.RunService.RenderStepped:Connect(function()
-    for _,a in pairs(game.Workspace.Players:GetChildren())do
-        if a.Name~=game.Players.LocalPlayer.Team.Name then
-            for _,b in pairs(a:GetChildren())do
-                b[HitBoxTarget].Size=Vector3.new(HitBoxSize,HitBoxSize,HitBoxSize)
-            end
-        end
+local Players = workspace.Players
+ 
+local OldIndex = nil
+ 
+OldIndex = hookmetamethod(Players, "__index", function(Self, Key)
+    if not checkcaller() and tostring(Self) == "Torso" and Key == "Size" then
+        return Vector3.new(10, 10, 10)
     end
+    return OldIndex(Self, Key)
 end)
